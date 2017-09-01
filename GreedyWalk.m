7cecbe487b2d103a77a67de8bdd3c332e75b94c3
Nelson Cook <nelson.cook98@gmail.com>
2017-09-01 19:48:34 +1200

    fixed easting handling

--- GreedyWalk.m
+++ GreedyWalk.m
@@ -7,15 +7,25 @@ function [rows,columns] = GreedyWalk(startpos,easting,heights)
 %Outputs: rows, columns: matched pair of vectors listing route taken by
 %         sequential row and column pairs
 
+%set basic variables
 mstart = startpos(1);
 nstart = startpos(2);
 [height,width] = size(heights);
 
+%Find number of times to call GreedyPick
+if easting == 1
+    traverse = abs(nstart-width) + 1;
+elseif easting == -1
+    traverse = nstart;
+else 
+    error('Expected easting to be 1 or -1, got %1.0f',easting)
+end
+%initialise first position
 tpos = startpos;
 rows(1) = startpos(1);
-columns(1) = startpos(1);
+columns(1) = startpos(2);
 
-for x = 2:(abs(nstart-width) + 2)
+for x = 2:traverse %Call greedypick right number of times (hopefull)
     tpos = GreedyPick(tpos,easting,heights);
     rows(x) = tpos(1);
     columns(x) = tpos(2);
