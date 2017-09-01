a32295c8c0be9363597ce01cf37c36193029db7c
Nelson Cook <nelson.cook98@gmail.com>
2017-09-01 16:34:42 +1200

    fixed a lil bug

--- FindSmallestElevationChange.m
+++ FindSmallestElevationChange.m
@@ -20,6 +20,7 @@ for x = 1:length(avail)
             leastElevations(1) = x; %Create vector
         end
     elseif tdist < mindist %we have a new least distance
+        mindist = tdist;
         clear leastElevations %stop the presses, throw everything out
         leastElevations(1) = x; %lets start again
     end
