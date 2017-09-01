803e04702522f291fda2e858bd5e2eae484a3bbb
Nelson Cook <nelson.cook98@gmail.com>
2017-09-01 19:46:30 +1200

    formatting

--- GreedyPick.m
+++ GreedyPick.m
@@ -20,19 +20,11 @@ elseif mpos == height %current position at bottom of array
     availposes(3) = inf; %Will never return as least distance
 else %generate avail positions automatically
     for x = 1:3
-        %disp(x)
-        %disp(mpos + easting)
-        %disp(npos - 2 + x)
         availposes(x) = heights(mpos - 2 + x, npos + easting);
     end
 end
 
 idxnext = FindSmallestElevationChange(heights(mpos,npos),availposes);
-
-%disp(currentpos)
-%disp(heights(mpos,npos))
-%disp(availposes)
-disp(idxnext)
 
 nextpos(1) = mpos - 2 + idxnext(1);
 nextpos(2) = npos + easting;
