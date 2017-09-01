e4b94729b746eeb05d1797cb7136e924eea1ef83
Nelson Cook <nelson.cook98@gmail.com>
2017-09-01 16:35:13 +1200

    Finished greedypick, added edge handling

--- GreedyPick.m
+++ GreedyPick.m
@@ -5,17 +5,36 @@ function [nextpos] = GreedyPick(currentpos, easting, heights)
 %           heights:    Elevation data for the whole matrix/area
 %Outputs:   nextpos:    1x2 Vector of next row and column
 
-mpos = currentpos(1);
-npos = currentpos(2);
+mpos = currentpos(1); %Current row
+npos = currentpos(2); %current column
 
+[height, width] = size(heights); %heightfield size
 
-%NEEDS HANDLING FOR EDGE CASES, i.e when you're at the top or bottom
-for x = 1:3
-    availposes(x) = heights(mpos + easting, npos - 2 + x)
+if mpos == 1 %current position at top of array
+    availposes(1) = inf; %Will never return as least distance
+    availposes(2) = heights(mpos,npos + easting);
+    availposes(3) = heights(mpos + 1, npos + easting);
+elseif mpos == height %current position at bottom of array
+    availposes(1) = heights(mpos - 1, npos + easting);
+    availposes(2) = heights(mpos,npos + easting);
+    availposes(3) = inf; %Will never return as least distance
+else %generate avail positions automatically
+    for x = 1:3
+        %disp(x)
+        %disp(mpos + easting)
+        %disp(npos - 2 + x)
+        availposes(x) = heights(mpos - 2 + x, npos + easting);
+    end
 end
 
-nextnpos = FindSmallestElevationChange(heights(mpos,npos),availposes);
-disp(nextnpos)
+idxnext = FindSmallestElevationChange(heights(mpos,npos),availposes);
 
+disp(currentpos)
+disp(heights(mpos,npos))
+disp(availposes)
+disp(idxnext)
+
+nextpos(1) = mpos - 2 + idxnext;
+nextpos(2) = npos + easting;
 
 end
