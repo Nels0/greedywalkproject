9ebf87490ac398f7182d53f87095c19dfca300b7
Nelson Cook <nelson.cook98@gmail.com>
2017-09-15 09:23:24 +1200

    Created adjacency matrix function

---
+++ createWeightedAdjMatrix.m
@@ -0,0 +1,36 @@
+function [adjmatrix] = createWeightedAdjMatrix(heights)
+%Creates an adjacency matrix with cost as adjacency value
+%Inputs:  heights: Matrix height field
+%Outputs: adjmatrix: Matrix where value in cell indicates path from row
+%           node to column node with cost of the value
+
+[inheight,inwidth] = size(heights);
+%preallocate array
+adjmatrix(inheight*inwidth) = 0;
+
+
+for m = 1:inheight
+    for n = 1:inwidth
+        row = (m-1) * inheight + n; 
+        if n ~= inwidth %not at end
+            %set horizontal cost
+                tcost = abs(heights(m,n) - heights(m,n+1));
+                tcol = (m-1) * inheight + n + 1;
+                adjmatrix(row,tcol) = tcost;
+            if m ~= 1 %not on top edge
+                %set diag up cost
+                tcost = abs(heights(m,n) - heights(m-1,n+1));
+                tcol = (m-1) * inheight + n;
+                adjmatrix(row,tcol) = tcost;
+            end
+            if m ~= inheight %not on bottom edge
+                %set diag down cost
+                tcost = abs(heights(m,n) - heights(m + 1,n+1));
+                tcol = (m-1) * inheight + n + 2;
+                adjmatrix(row,tcol) = tcost;
+            end 
+        end
+    end
+end
+
+end
