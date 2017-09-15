4f5b50f4d567ca3f637f597a394934f82bb7475e
Nelson Cook <nelson.cook98@gmail.com>
2017-09-15 16:26:14 +1200

    added column handling to reverse()

--- Reverse.m
+++ Reverse.m
@@ -1,6 +1,12 @@
 function r = Reverse(vector)
-len = length(vector);
-r = zeros(1,len);
-for x = 1:len
-    r(x) = vector(len - x + 1);
+[height, width] = size(vector);
+r = zeros(height,width);
+if height == 1
+    for x = 1:width
+        r(x) = vector(width - x + 1);
+    end
+elseif width == 1
+    for x = 1:height
+        r(x,1) = vector(height - x + 1);
+    end
 end
