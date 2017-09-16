1fa2c1d72c8ae75b2fabf38c7bdd48d309f81202
Nelson Cook <nelson.cook98@gmail.com>
2017-09-17 00:33:16 +1200

    code commenting and cleanup

--- BestGreedyPath.m
+++ BestGreedyPath.m
@@ -4,28 +4,30 @@ function [rows,columns,elevations] = BestGreedyPath(heights)
 %inputs:    Heights: matrix of heightfield
 %outputs:   Rows: row indices in order
 %           Columns: column indices in order
-%           Elevations: log of heights travelled through
+%           Elevations: sequnce of heights travelled through
 
 
 %the basic stuff
 [height, width]  = size(heights);
-
+%initialise min cost to infinity so any cost will be smaller
 mincost = inf;
 
+%Iterate over whole matrix
 for m = 1:height
     for n = 1:width
-        if n < width
+        if n < width %Go east if you can
             [trowsE,tcolsE] = GreedyWalk([m n],1,heights);
         end
-        if n > 1
+        if n > 1 %Go west if you can
             [trowsW,tcolsW] = GreedyWalk([m n],-1,heights);
+            %put west-heading sequnces the right way around
             trowsW = flip(trowsW);
             tcolsW = flip(tcolsW);
         end
-        if n > 1 && n < width
-            trows = [trowsW trowsE(2:end)];
+        if n > 1 && n < width %concatenate arrays if necessary
+            trows = [trowsW trowsE(2:end)]; %trim duplicate values
             tcols = [tcolsW tcolsE(2:end)];
-        elseif n == 1
+        elseif n == 1 %otherwise just save values
             trows = trowsE;
             tcols = tcolsE;
         elseif n == width
@@ -33,14 +35,16 @@ for m = 1:height
             tcols = tcolsW;
         end
         
+        %check cost
         [televations, tcost] = FindPathElevationsAndCost(trows,tcols,heights);
         
+        %save output if the current path is the cheapest one
         if tcost < mincost
             rows = trows;
             columns = tcols;
             elevations = televations;
             mincost = tcost;
-        end            
+        end
     end
 end
 end
--- Reverse.m
+++ Reverse.m
@@ -1,12 +1,21 @@
 function r = Reverse(vector)
+%Reverses a 1d array, like flip() but for only 1d vectors
+%Input: Vector (1xn or nx1 matrix)
+%Output: Vector in reverse order (same dimension as input matrix)
+
+
 [height, width] = size(vector);
+%initialise array to same size for speed
 r = zeros(height,width);
-if height == 1
+
+if height == 1 %row vector
     for x = 1:width
-        r(x) = vector(width - x + 1);
+        r(x) = vector(width - x + 1);   %set xth element of out to
+                                        %xth from the end of input array
     end
-elseif width == 1
+elseif width == 1 %column vector
     for x = 1:height
-        r(x,1) = vector(height - x + 1);
+        r(x,1) = vector(height - x + 1);%set xth element of out to
+                                        %xth from the end of input array
     end
 end
