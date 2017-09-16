ea9ce395de1377178dc35edba7080a7cda4c4603
Nelson Cook <nelson.cook98@gmail.com>
2017-09-16 23:45:35 +1200

    basically made it the same as the first try

--- GreedyWalk.m
+++ GreedyWalk.m
@@ -1,21 +1,28 @@
 function [rows,cols] = GreedyWalk(pos,easting,heights)
 
-loop = true;
 row_t = pos(1);
 col_t = pos(2);
-tpos = pos;
-x = 1;
-while loop
-   try
-       [tpos] = GreedyPick(tpos, easting, heights); 
-       rows(x) = row_t;
-       cols(x) = col_t;
-       row_t = tpos(1);
-       col_t = tpos(2);
-       x = x + 1;
-   catch
-       loop = false;
-   end
+t_pos = pos;
+
+[~,width] = size(heights);
+
+if easting == 1
+    traverse = width - col_t + 1;
+elseif easting == -1
+    traverse = col_t;
+    
+end
+
+rows = zeros(1,traverse);
+cols = zeros(1,traverse);
+
+rows(1) = row_t;
+cols(1) = col_t;
+
+for x = 2:traverse
+    [t_pos] = GreedyPick(t_pos, easting, heights);
+    rows(x) = t_pos(1);
+    cols(x) = t_pos(2);
 end
 
 end
