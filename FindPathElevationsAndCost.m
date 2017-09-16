cbbb1a963f174d49a3babc22ed5f4fb384219eb0
Nelson Cook <nelson.cook98@gmail.com>
2017-09-17 01:27:07 +1200

    final 7funcs code cleanup

--- FindPathElevationsAndCost.m
+++ FindPathElevationsAndCost.m
@@ -5,14 +5,17 @@ function [pathElevations, totalCost] = FindPathElevationsAndCost(rows,columns,he
 %Outputs: pathelevations: height at each point in path
 %         totalcost: sum of height differences between path points
 
-len = length(rows);
+len = length(rows); %save length(rows) for simplicity
+pathElevations = zeros(1,len); %preallocate vector for speed
 
-pathElevations = zeros(1,len);
 for x = 1:len
+    %store elevations for each row/column pair
     pathElevations(x) = heights(rows(x),columns(x));
 end
 
+%create arrays that are shifted by one and trimmed by one
 elev1 = [pathElevations(2:end)];
 elev2 = [pathElevations(1:end-1)];
 
+%sum absolute differences between shifted matrices
 totalCost = sum(abs(elev1 - elev2));
--- FindSmallestElevationChange.m
+++ FindSmallestElevationChange.m
@@ -1,10 +1,17 @@
 function [leastindices] = FindSmallestElevationChange(cheight,nheights)
+%Given number a, and vector b, returns positions of b that have
+%least distance from a
+%Inputs:    a: float/int current elevation
+%           b: array of elevations
+%Outputs:   leastElevations: vector of indices of least distance values
 
-deltas = zeros(1,length(nheights));
+deltas = zeros(1,length(nheights)); %preallocate height differences
 
 for x = 1:length(nheights)
+    %calculate min height differences
     deltas(x) = abs(cheight - nheights(x));
 end
 
- leastindices = find(deltas == min(deltas));
+%find indices where values are minimum
+leastindices = find(deltas == min(deltas));
 end
