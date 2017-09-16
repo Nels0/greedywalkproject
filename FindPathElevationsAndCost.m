e1348778249ad0b19e9de775f50d864e54f17cdc
Nelson Cook <nelson.cook98@gmail.com>
2017-09-16 22:59:20 +1200

    vectorised ElevationsAndCost

--- FindPathElevationsAndCost.m
+++ FindPathElevationsAndCost.m
@@ -1,27 +1,18 @@
-function [pathelevations, totalcost] = FindPathElevationsAndCost(rows,columns,heights)
+function [pathElevations, totalCost] = FindPathElevationsAndCost(rows,columns,heights)
 %Outputs heights for each step of path, and total cost
 %Inputs:  rows, columns: sequential path coords
 %         heights: heightfield in matrix form
 %Outputs: pathelevations: height at each point in path
 %         totalcost: sum of height differences between path points
 
+len = length(rows);
 
-%initialise
-lastheight = heights(rows(1),columns(1));
-totalcost = 0;
-
-pathelevations = zeros(1,length(rows));%Preallocate vector for speed
-
-if length(rows) == length(columns) %give clear error messages in case
-    
-    for x = 1:length(rows) %iterate over coord
-        currentheight = heights(rows(x),columns(x)); %get current height
-        pathelevations(x) = currentheight; %log heights
-        totalcost = totalcost + abs(currentheight - lastheight); %update cost
-        lastheight = currentheight; %for finding height delta
-    end
-else
-    
-    error('Row/Column coordinate vectors length mismatch')
+pathElevations = zeros(1,len);
+for x = 1:len
+    pathElevations(x) = heights(rows(x),columns(x));
 end
-end
+
+elev1 = [pathElevations(2:end)];
+elev2 = [pathElevations(1:end-1)];
+
+totalCost = sum(abs(elev1 - elev2));
