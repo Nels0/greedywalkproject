8861fdd0ce43cc3af31e86360852d655635d7da6
Nelson Cook <nelson.cook98@gmail.com>
2017-09-16 21:01:37 +1200

    reworked greedypick to selectively execute

--- GreedyPick.m
+++ GreedyPick.m
@@ -1,23 +1,32 @@
 function [nextpos] = GreedyPick(pos, easting, heights)
 
-
+[height,~] = size(heights);
 row = pos(1);
 col = pos(2);
-
-avail = inf(1,3);
+cheight = heights(row,col);
+%avail = inf(1,3);
+tmin = inf;
 
 for x = 1:3
-    try
-        avail(x) = heights(row + x - 2, col + easting);
-    catch
+    if (row + x - 2) > 1 && (row + x - 2) <= height
+        %disp("bong")
+        %avail(x) = heights(row + x - 2, col + easting);
+        tm = row + x - 2;
+        tn = col + easting;
+        tavail = heights(tm,tn);
+        cdistance = abs(cheight - tavail);
+        if cdistance < tmin
+            %disp("bing")
+            idx = x;
+        end
     end
 end
 
-height = heights(row,col);
+%height = heights(row,col);
 
-idx = FindSmallestElevationChange(height,avail);
+%idx = FindSmallestElevationChange(height,avail);
 
-mnext = row - 2 + idx(1);
+mnext = row - 2 + idx;
 nnext = col + easting;
 
 nextpos = [mnext, nnext];
