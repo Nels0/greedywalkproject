34fd16f2e7aa5cc10df4f649e697e2ce993cc7b6
Nelson Cook <nelson.cook98@gmail.com>
2017-09-17 14:10:22 +1200

    Updated profiler to provide average run time

--- profiler.m
+++ profiler.m
@@ -1,4 +1,11 @@
 %pick = BestGreedyPath(E)
-tic
-[pathRow,pathCol,pathElev] = BestGreedyPath(E);
-toc
+num = 6;
+E = peaks(100);
+t_x = zeros(1,num);
+for x = 1:num
+    E = peaks(100);
+    tic
+    [pathRow,pathCol,pathElev] = BestGreedyPath(E);
+    t_(x) = toc;
+end
+disp(mean(t_))
